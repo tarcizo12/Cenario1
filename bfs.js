e2621107fc9ddb38ca0a7de6e6c51f3b49357b6a
Tarcizo Neto <tarcizon12@gmail.com>
2023-03-12 21:16:10 -0300

    Algoritimo de BFS

--- bfs.js
+++ bfs.js
@@ -61,34 +61,60 @@ const grafoTeste = {
     'B': ['C']
 }
 
-function visitaNo(grafo, inicio, fim){
-    const visitados = [inicio]
+function bfs(grafo, inicio, fim) {
+    // Cria um dicionário para armazenar os pais de cada nó visitado
+    let pais = {};
+    // Cria uma fila para armazenar os nós que serão visitados
+    let fila = [];
+    // Adiciona o nó inicial na fila e define o pai como null
+    fila.push(inicio);
+    pais[inicio] = null;
     
-
-    for(keys in grafo){
-        for(index in grafo[keys]){
-            const current = grafo[keys][index]
-            
-            if(!visitados.includes(current)){
-                visitados.push(current)
-                
-                //console.log(current)
-                
-                if(current == fim){
-                    return visitados
-                }
-            }
-            
+    // Loop principal da busca em largura
+    while (fila.length > 0) {
+      // Remove o primeiro nó da fila
+      let atual = fila.shift();
+      // Verifica se o nó atual é o nó final
+      if (atual == fim) {
+        // Se for, constrói o caminho até o nó inicial
+        let caminho = [];
+        while (atual != null) {
+          caminho.unshift(atual);
+          atual = pais[atual];
         }
+        // Retorna o caminho encontrado
+        return caminho;
+      }
+      // Se o nó atual não for o nó final, visita seus vizinhos
+      let vizinhos = grafo[atual];
+      for (let i = 0; i < vizinhos.length; i++) {
+        let vizinho = vizinhos[i];
+        // Verifica se o vizinho já foi visitado
+        if (!(vizinho in pais)) {
+          // Se não foi, marca o vizinho como visitado, adiciona na fila e define o pai
+          pais[vizinho] = atual;
+          fila.push(vizinho);
+        }
+      }
     }
-
-    console.log(visitados)
-}
-
-
-
-//const algoritmoMenorCaminho = BFS(grafo1, 'B', 'A');
+    // Se o nó final não foi encontrado, retorna null
+    return null;
+  }
 
 
 
-console.log(visitaNo(graph,'A','F'))
+  let grafo = {
+    "A": ["B", "C"],
+    "B": ["A", "C", "D"],
+    "C": ["A", "B", "D", "E"],
+    "D": ["B", "C", "E", "F"],
+    "E": ["C", "D"],
+    "F": ["D"]
+  };
+  
+  let inicio = "A";
+  let fim = "F";
+  
+  let caminho = bfs(grafo, inicio, fim);
+  
+  console.log("Caminho encontrado:", caminho);
