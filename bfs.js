22a344dc3fe662777d23c72ccc9d6a267feab7ca
Tarcizo Neto <tarcizon12@gmail.com>
2023-03-12 21:18:56 -0300

    Isso aqui funciona

--- bfs.js
+++ bfs.js
@@ -1,73 +1,13 @@
-//algoritmo de Busca em Largura, ou BFS, visita todos os vértices de um grafo começando pelos adjacentes e indo de forma sucessiva (?). 
-
-//grafo representado por uma matriz de adjacência
-
-//assumindo que são quantos vértices? 10
-
-
-function BFS (grafo, comeco, fim){
-    //lista de vértices que serão visitados
-    const verticesAVisitar = [];
-    
-    //set como uma coleção dos vértices visitados, apenas uma vez assumindo que ele vai escolher um caminho de um ponto a outro sem repetir os vértices (?)
-    const visitados = new Set();
-    
-    //caminho
-    const caminho = {};
-    
-    //adicionando vértice de origem nos verticesAVisitar e marcando como visitado no set
-    verticesAVisitar.push(comeco);
-    visitados.add(comeco);
-    
-    //iterando nos verticesAVisitar
-    while(verticesAVisitar.lenght > 0){
-        const verticeAtual = verticesAVisitar.shift();
-        if (verticeAtual === fim){
-            //chegamos ao final, falta so construir o caminho e retornar
-            const resultado = [];
-            while (verticeAtual !== comeco){
-                resultado.push(verticeAtual);
-                verticeAtual = caminho[verticeAtual]
-            }
-            resultado.push(comeco);
-            return resultado.reverse(); 
-        }
-        for (const vizinho of grafo[verticeAtual]){
-            if (!visitados.has(vizinho)){
-                verticesAVisitar.push(vizinho);
-                visitados.add(vizinho);
-                caminho[vizinho] = verticeAtual;
-            }
-        }
-        
-    }
-    
-    return null;
-    
-}
-
-
-const graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F'],
-    'D': [],
-    'E': ['F'],
-    'F': []
-}
-
-const grafoTeste = {
-    'A': ['B', 'C'],
-    'B': ['C']
-}
-
 function bfs(grafo, inicio, fim) {
     // Cria um dicionário para armazenar os pais de cada nó visitado
     let pais = {};
+
     // Cria uma fila para armazenar os nós que serão visitados
     let fila = [];
+
     // Adiciona o nó inicial na fila e define o pai como null
     fila.push(inicio);
+
     pais[inicio] = null;
     
     // Loop principal da busca em largura
@@ -85,6 +25,8 @@ function bfs(grafo, inicio, fim) {
         // Retorna o caminho encontrado
         return caminho;
       }
+
+
       // Se o nó atual não for o nó final, visita seus vizinhos
       let vizinhos = grafo[atual];
       for (let i = 0; i < vizinhos.length; i++) {
@@ -97,6 +39,8 @@ function bfs(grafo, inicio, fim) {
         }
       }
     }
+
+    
     // Se o nó final não foi encontrado, retorna null
     return null;
   }
